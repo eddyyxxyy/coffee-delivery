a194d2a9806bd262dee9ba66e8d5161cc8da8b89
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-11-30 18:43:13 -0300

    feat(pages): :mag: defines title, meta description and other meta-tags with react-helmet

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,30 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
+
 export function Home() {
-  return <h1>Home Page</h1>;
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>{t("homeTitle")}</title>
+          <meta name="description" content={t("homeMetaDesc")} />
+          <meta
+            property="og:title"
+            content="Coffee Delivery - Special and Unique | eddyyxxyy"
+          />
+          <meta
+            property="og:description"
+            content="Discover Coffee Delivery, our fictitious coffee platform! Get to know our coffees and fall in love with our selection. Try buying coffee with us!"
+          />
+          <link rel="canonical" href="https://www.tobeimplemented.com" />
+        </Helmet>
+      </HelmetProvider>
+
+      <main className="flex-grow overflow-y-auto">
+        <h1>Home Page</h1>
+      </main>
+    </>
+  );
 }
