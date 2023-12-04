5d9c1c221f2f98ea17b4d2cb1a5dfc2001ca0ad8
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-12-04 19:06:59 -0300

    feat(utils): :sparkles: creates functions to handle currency exchange between $ and R$

---
+++ src/utils/convertToDollar.ts
@@ -0,0 +1,26 @@
+interface APIData {
+  rates: {
+    USD: number;
+  };
+}
+
+async function getDollarQuotation(): Promise<number | null> {
+  try {
+    const response = await fetch(
+      "https://api.exchangerate-api.com/v4/latest/BRL",
+    );
+    const data = (await response.json()) as APIData;
+
+    return data.rates.USD;
+  } catch {
+    return null;
+  }
+}
+
+export async function convertToDollars(value: number): Promise<string> {
+  const quotation = await getDollarQuotation();
+
+  return quotation !== null
+    ? (value * quotation).toFixed(2)
+    : value.toString();
+}
