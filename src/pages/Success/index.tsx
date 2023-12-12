9cc8776ade7690f84500f361e7efc36f4109ff1c
Edson Pimenta <dev.eddyyxxyy@gmail.com>
2023-12-12 16:44:56 -0300

    feat(pages): :sparkles: finishes Success page UI and functionality

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,6 +1,8 @@
-import { useContext } from "react";
+import { CurrencyDollar, MapPin, Timer } from "@phosphor-icons/react";
+import { useContext, useEffect } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 import successPageDeliveryGuy from "../../assets/success-page-delivery-guy.svg";
 import { AppContext } from "../../contexts/AppContext";
@@ -8,6 +10,13 @@ import { AppContext } from "../../contexts/AppContext";
 export function Success() {
   const { orderInfo } = useContext(AppContext);
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (orderInfo === null) {
+      navigate("/");
+    }
+  }, [orderInfo, navigate]);
 
   return (
     <>
@@ -29,7 +38,7 @@ export function Success() {
       </HelmetProvider>
 
       <main className="flex-grow overflow-y-auto px-4 pb-24 font-sans-r md:px-14 lg:px-36">
-        <div className="px-4 pt-7 md:pt-14 lg:pt-20">
+        <div className="flex flex-col items-center px-4 pt-7 md:pt-14 lg:block lg:pt-20">
           <div className="lg:grid lg:grid-cols-2 lg:gap-x-24">
             <div>
               <h1 className="font-sans-b text-2xl font-extrabold text-product-yellow-dark md:text-3xl lg:text-[2rem]">
@@ -41,11 +50,63 @@ export function Success() {
             </div>
           </div>
           <div className="grid grid-cols-1 gap-y-9 md:gap-y-16 lg:grid lg:grid-cols-2 lg:gap-x-24">
-            <div className="grid grid-rows-3 gap-y-8">
-              <div className="flex items-center gap-3"></div>
+            <div className="relative rounded-bl-[2.25rem] rounded-br-md rounded-tl-md rounded-tr-[2.25rem] p-10">
+              <div
+                className="absolute -inset-px rounded-bl-[2.25rem] rounded-br-md rounded-tl-md rounded-tr-[2.25rem] bg-gradient-to-r from-yellow-400 to-pink-400"
+                aria-hidden="true"
+              ></div>
+              <div
+                className="absolute inset-0 rounded-bl-[2.25rem] rounded-br-md rounded-tl-md rounded-tr-[2.25rem] bg-base-bg"
+                aria-hidden="true"
+              ></div>
+              <div className="relative z-10 flex h-full flex-col items-center justify-between gap-8 md:items-start">
+                <div className="flex w-full flex-col items-center justify-center gap-3 text-base-text md:flex-row">
+                  <div className="w-fit rounded-full bg-product-purple p-2">
+                    <MapPin size={16} weight="fill" className="text-white" />
+                  </div>
+                  <p className="w-full text-center md:text-left">
+                    {t("shippingIn")}{" "}
+                    <span className="font-bold">
+                      {orderInfo?.address.street}, {orderInfo?.address.number}
+                    </span>
+                    <span className="block">
+                      {orderInfo?.address.neighborhood} -{" "}
+                      {orderInfo?.address.city}, {orderInfo?.address.state}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex w-full flex-col items-center justify-center gap-3 text-base-text md:flex-row">
+                  <div className="w-fit rounded-full bg-product-yellow p-2">
+                    <Timer size={16} weight="fill" className="text-white" />
+                  </div>
+                  <p className="w-full text-center md:text-left">
+                    {t("estimatedWait")}
+                    <span className="block font-bold">20 min - 30 min</span>
+                  </p>
+                </div>
+                <div className="flex w-full flex-col items-center justify-center gap-3 text-base-text md:flex-row">
+                  <div className="w-fit rounded-full bg-product-yellow-dark p-2">
+                    <CurrencyDollar
+                      size={16}
+                      weight="bold"
+                      className="text-white"
+                    />
+                  </div>
+                  <p className="w-full text-center md:text-left">
+                    {t("paymentOnDeliverySuccessPage")}
+                    <span className="block font-bold">
+                      {t(`${orderInfo?.paymentType}PaymentSuccessMessage`)}
+                    </span>
+                  </p>
+                </div>
+              </div>
             </div>
-            <div>
-              <img src={successPageDeliveryGuy} alt="" />
+            <div className="max-h-72">
+              <img
+                src={successPageDeliveryGuy}
+                alt=""
+                className="h-full w-full"
+              />
             </div>
           </div>
         </div>
